2678c98eb0910d6eb55ad7981df23e062a645792
Marcus Nygaard <marnyg123@gmail.com>
2023-11-09 10:31:50 +0100

    finish making target pipeline

---
+++ R/functions.R
@@ -0,0 +1,13 @@
+#' Calculate descriptive statistics of each metabolite.
+#'
+#' @param data Lipidomics dataset.
+#'
+#' @return A data.frame/tibble.
+#'
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) %>%
+        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(.x, digits = 1)))
+}
+
